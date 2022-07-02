1c8b8d9d1aab90ee5df775d28333380383c9af96
Rorroh <rorroh@live.com>
2022-07-01 21:13:18 -0500

    Add project files.

---
+++ ChatQuickView.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace ChatQuickView
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    public class ChatQuickView : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "Rorroh";
+        public const string PluginName = "ChatQuickView";
+        public const string PluginVersion = "1.0.0";
+
+        private static LocalUser localUser;
+
+        public void Awake()
+        {
+            On.RoR2.Run.Start += (orig, self) =>
+            {
+                localUser = LocalUserManager.GetFirstLocalUser();
+                orig(self);
+            };
+
+            On.RoR2.UI.ChatBox.UpdateFade += (orig, self, dt) =>
+            {
+                if (localUser.inputPlayer.GetButton("info"))
+                    self.ResetFadeTimer();
+                orig(self, dt);
+            };
+        }
+    }
+}
